048a00a3cf4f0c71bf5eab702ede25da23201220
Abdull Rehman <abdullrehman.ds@gmail.com>
2024-04-17 19:55:41 +0500

    indexing of lines

--- 7- Menu/script.js
+++ 7- Menu/script.js
@@ -6,14 +6,14 @@ xhhtp.onreadystatechange = function () {
     menu = this.responseText;
     menu = JSON.parse(menu);
   }
-}
+};
 xhhtp.open("GET", "./data.json", false);
 xhhtp.send();
 
 window.addEventListener("DOMContentLoaded", function () {
   displayItems(menu);
   displayFilterButtons();
-})
+});
 
 // targeting items container
 let itemsContainer = document.querySelector(".menu-items");
@@ -33,7 +33,7 @@ function displayItems(menuItems) {
         ${item.desc}
       </p>
     </div>
-  </article>`
+  </article>`;
   });
 
   // join function will convert the multiple strings into one string by removing ,
@@ -47,21 +47,26 @@ let filterBtnsContainer = document.querySelector(".filter-buttons");
 // writing function that will do the following tasks:
 // 1- filtering and making an array of unique categories
 // 2- displaying the filtered category buttons in the filter button container
-// 3- 
+// 3-
 function displayFilterButtons() {
   // making array of unique categories
-  let filterBTns = menu.reduce(function (values, item) {
-    if (!values.includes(item.category)) {
-      values.push(item.category)
-    }
-    return values;
-  }, ["All"])
+  let filterBTns = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["All"]
+  );
 
   // displaying buttons in buttons container
-  filterBTns = filterBTns.map(function (btn) {
-    let btnHtml = `<button class="filter-button" data-id=${btn}>${btn}</button>`
-    return btnHtml;
-  }).join("");
+  filterBTns = filterBTns
+    .map(function (btn) {
+      let btnHtml = `<button class="filter-button" data-id=${btn}>${btn}</button>`;
+      return btnHtml;
+    })
+    .join("");
 
   filterBtnsContainer.innerHTML = filterBTns;
 
@@ -71,7 +76,6 @@ function displayFilterButtons() {
 
   // traversing each button
   filterBtns.forEach(function (btn) {
-
     // adding event listener to each button
     btn.addEventListener("click", (e) => {
       let category = e.currentTarget.dataset.id;
@@ -83,12 +87,9 @@ function displayFilterButtons() {
 
       if (category === "All") {
         displayItems(menu);
-      }
-      else {
+      } else {
         displayItems(filteredItems);
       }
-
-    })
-  })
-
+    });
+  });
 }
